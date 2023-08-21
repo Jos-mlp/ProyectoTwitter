177977461266bdc6910c690afc84e1224d8e88b2
Josue Maldonado <Memejosue2012@hotmail.com>
2023-08-21 15:49:50 -0600

    Cambios en la version de tweepy

--- give_me_tweets.py
+++ give_me_tweets.py
@@ -1,50 +1,31 @@
-import credentials
 import tweepy
-import time
+import pandas as pd
+import credentials
 
-# Autenticación e ingreso
-auth = tweepy.OAuthHandler(credentials.API_KEY, credentials.API_SECRET_KEY)
-auth.set_access_token(credentials.ACCESS_TOKEN, credentials.ACCESS_TOKEN_SECRET)
+# Se pasa la clave de autenticación de la API de Twitter
+auth = tweepy.OAuth1UserHandler(
+    credentials.API_KEY, credentials.API_SECRET_KEY,
+    credentials.ACCESS_TOKEN, credentials.ACCESS_TOKEN_SECRET
+)
 
-api = tweepy.API(auth)
+# Se instancia la API de Tweepy
+api = tweepy.API(auth, wait_on_rate_limit=True)
 
+# Consulta de búsqueda para tweets relacionados con guatemala, excluyendo retweets, respuestas y enlaces
+search_query = "'ref''guatemala'-filter:retweets AND -filter:replies AND -filter:links"
+no_of_tweets = 100
 
-# Probando la conexión
-# public_tweets = api.home_timeline()
-# for tweet in public_tweets:
-    # print(f'{tweet.user.screen_name}:\ntweet.text\n{"*"*60}')
+try:
+     # Se intenta obtener un número determinado de tweets relacionados con la búsqueda
+    tweets = api.search_tweets(q=search_query, lang="es", count=no_of_tweets, tweet_mode ='extended')
+    
+   # Extracción de algunos atributos de los tweets y almacenamiento en una lista
+    attributes_container = [[tweet.user.name, tweet.created_at, tweet.favorite_count, tweet.source, tweet.full_text] for tweet in tweets]
 
-
-# Obtener los tweets de un topic
-id = None
-count = 0
-while True:
-    tweets = api.search_tweets(q='gt', tweet_mode='extended', max_id=id)
-    for tweet in tweets:
-        if tweet.full_text.startswith('RT'):
-            count += 1
-            continue
-        f = open('./prueba.txt', 'a', encoding='utf-8')
-        
-        tweet_id = tweet.id #Id tweet int
-        tweet_texto = tweet.full_text #texto tweet
-        tweet_Source = tweet.source #Desde donde tweetean
-        tweet_Created = tweet.created_at #Fecha de creacion del usuario
-        user_id = tweet.user.id #ID usuario
-        user_name = tweet.user.name #Nombre del usuario
-        user_Create_Dt = tweet.user.created_at #Fecha de creacion del usuario
-        userDesc = tweet.user.description #Descripcion del usuario
-        userFollowerCt = tweet.user.followers_count #Personas a las que sigue la cuenta
-        userFriendsCt = tweet.user.friends_count #Personas que siguen a la cuenta
-        userLocation = tweet.user.location #Ubicacion del usuario
-
-        f.write("Tweet_id: " + str(tweet_id)  + '\n' + "Texto tweet: " +  tweet_texto + '\n'+ "tweet_Source :  " + str(tweet_Source) + '\n'
-        + "tweet_Created: " + str(tweet_Created) + '\n' + "user_id: " + str(user_id) + '\n' + "User_Name: " + str(user_name) + '\n'
-        + "user_Create_Dt: " + str(user_Create_Dt) + '\n' + "userDesc: " + str(userDesc) + '\n' + "userFollowerCt: " + str(userFollowerCt) + '\n'
-        + "userFriendsCt: " + str(userFriendsCt) + '\n' + "userLocation: " + str(userLocation) + '\n' + "userTimezone: " '\n'+ '*'*50 + '\n') # usa esto si quieres tener separados los tweets
-        f.close
-        count += 1
-        id = tweet.id
-    print(count)
-    print(id)
-    time.sleep(1)
+    # Lista de nombres de columnas para renombrar en el DataFrame
+    columns = ["User", "Date Created", "Number of Likes", "Source of Tweet", "Tweet"]
+    
+    # Creación del DataFrame utilizando los datos extraídos
+    tweets_df = pd.DataFrame(attributes_container, columns=columns)
+except BaseException as e:
+    print('Status Failed On,',str(e))
--- mostrar.py
+++
@@ -1,9 +0,0 @@
-from mongodb import Mongo
-mongo = Mongo()
-diccionario = {
-    "llave": "contenido"
-}
-
-mongo.insert(diccionario)
-
-mongo.insert_T(diccionario)
